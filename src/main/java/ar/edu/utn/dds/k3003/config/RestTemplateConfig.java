6917ca9a8539bd9bab8d953a046e199030b992fb
juan9772 <juanpirinto@hotmail.com>
2025-10-02 21:14:58 -0300

    Update RestTemplateConfig.java

--- src/main/java/ar/edu/utn/dds/k3003/config/RestTemplateConfig.java
+++ src/main/java/ar/edu/utn/dds/k3003/config/RestTemplateConfig.java
@@ -2,6 +2,7 @@ package ar.edu.utn.dds.k3003.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class RestTemplateConfig {
